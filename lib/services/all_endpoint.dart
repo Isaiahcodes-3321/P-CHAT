0192d186355d2bc583c5544fd7522d0e1844191a
Isaiahcodes-3321 <shellisaiah2020@gmail.com>
2025-06-11 10:50:47 +0100

    update on files

---
+++ lib/services/all_endpoint.dart
@@ -0,0 +1,11 @@
+// post service
+String registerEndpoint = '/api/v1/auth/signup';
+String verifyOTPEndpoint = '/api/v1/auth/verify-OTP';
+String verifyEmailEndpoint = '/api/v1/auth/resend-verification-email';
+String loginEndpoint = '/api/v1/auth/login';
+String forgotPasswordEndpoint = '/api/v1/auth/forgot-password';
+String refreshTokenEndpoint = '/api/v1/auth/refresh-token';
+String uploadPdfEndpoint = '/api/v1/chat/upload';
+
+// path service
+String resetPasswordEndpoint  = '/api/v1/auth/reset-password';
